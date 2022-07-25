70474993a8addba2825603c745a39b4edcd6867b
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2022-07-25 21:21:52 +0100

    New functions
    Adds New functions

--- amine.js
+++ amine.js
@@ -1,52 +1,68 @@
 const einput = document.getElementById("input"),
 	eoutput = document.getElementById("output");
-function interpret(){
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function interpret(){
 	eoutput.value = "";
 	let lines = einput.value.split("\n"),
 		variables = new Map(),
 		isUppercase = false;
 	for(let i = 0; i < lines.length; i++){
+		if(lines[i].startsWith("//")) continue;
 		let words = lines[i].split(" ");
 		switch(words[0]){
 			case "amine":
+				// SETLOWERCASE
 				// sets isUppercase flag to false
 				isUppercase = false;
 				break;
 			case "AMINE":
+				// SETUPPERCASE
 				// sets isUppercase flag to true
 				isUppercase = true;
 				break;
 			case "Amine":
+				// ASSIGN
 				// Assign variable to a value
 				isamine(words[1],words[2]);
 				variables.set(words[1],amineToNumber(words[2]));
 				break;
-			case "amiNe":
-				// print number from argument (might be useful for debugging)
-				isamine(words[1]);
-				eoutput.value += amineToNumber(words[1]);
-				break;
-			case "aminE":
-				// prints character from argument (might be useful for debugging)
-				isamine(words[1]);
-				eoutput.value += numberToChar(amineToNumber(words[1]),isUppercase);
-				break;
-			case "AmiNe":
+			case "aMine":
+				// PRINTNUM
 				// print number from variable
 				isamine(words[1]);
 				eoutput.value += variables.get(words[1]);
 				break;
-			case "AminE":
+			case "AMine":
+				// PRINTCHAR
 				// print char from variable
 				isamine(words[1]);
 				eoutput.value = numberToamine(variables.get(words[1]));
 				break;
-			case "AMIne":
-				// add to variables together and sets the third variable to the result
+			case "AmIne":
+				// ADD
+				// add two variables together and sets the third variable to the result
 				isamine(words[1],words[2],words[3])
-				variables.set(word[3],variables.get(words[1])+variables.get(words[2]));
+				const result = (variables.get(words[1]) + variables.get(words[2])) % 32
+				variables.set(words[3], result);
 				break;
-			
+			case "aMIne":
+				// JUMPBY
+				// jumps by the amount of line of code specified in the value of the first variable and uses the first bit of the second variable to determine whether to jump forwards or backwards 
+				isamine(words[1], words[2]);
+				i += (variables.get(words[1]) + 1) * ((variables.get(words[2]) % 1) ? -1 : 1)
+				// sleep 10 seconds so the website doesn't crash
+				await sleep(10);
+				break;
+			case "AmINE":
+				// DELETE
+				isamine(words[1]);
+				variables.delete(words[1]);
+				break;
+			case "aMINE":
+				// EXIT
+				return;
 		}
 	}
 }
@@ -61,6 +77,7 @@ function isamine(...strs){
 }
 function amineToNumber(amine){
 	// converts an amine to a number
+	// the first bit is on the right
 	let number = 0,
 		added = 1;
 	for(let i = 0; i < 5; i++){
